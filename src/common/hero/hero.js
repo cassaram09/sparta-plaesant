8556d856370153eec93deb7ba374220efe822827
cassaram09 <cassaram09@gmail.com>
2019-03-11 17:55:21 -0400

    adjust styles on hero component

--- src/common/hero/hero.js
+++ src/common/hero/hero.js
@@ -6,7 +6,7 @@ class Hero extends Component {
   render() {
     return (
       <div className="hero">
-      	<div className="hero__text">
+      	<div className="hero__text container">
       		<h1>Screening: <br/>DVF Secret Agent Part 2</h1>
       		<h3>October 15, 2017</h3>
       		<p>Join us for a private screening of DVF Secret Agent Part 2<br/>with our special guest, director Peter Lindberg.</p>
@@ -18,6 +18,8 @@ class Hero extends Component {
        		infiniteLoop={true}
        		autoPlay={true}
        		interval={5000}
+          transitionTime={500}
+          stopOnHover={false}
        	>
           <img src="https://cnet4.cbsistatic.com/img/DtSrStRVmFiah7YNTrJV3_dOBH4=/970x0/2018/11/21/a502f3f1-2a07-4f3c-966a-3ad7b3d6134e/rivian-r1t-11.jpg" />
           <img src="https://i.kinja-img.com/gawker-media/image/upload/s--xBUTOaFk--/c_scale,f_auto,fl_progressive,q_80,w_800/sptinjuhtlhfwadlp1oy.jpg" />
