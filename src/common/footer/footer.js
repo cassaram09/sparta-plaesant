f46e91c25635889c00a9a28d4496dd07fe2343b2
cassaram09 <cassaram09@gmail.com>
2019-03-12 09:17:56 -0400

    adjust styles of footer, header, and nav; adds nav overlay

--- src/common/footer/footer.js
+++ src/common/footer/footer.js
@@ -4,9 +4,9 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-    		<span className="footer__copyright">&copy; 2017 - Sparta Plaesant</span>&nbsp;
-    		<a className="footer__social-link -instagram" href="#">Instagram</a>&nbsp;
-    		<a className="footer__social-link -facebook" href="#">Facebook</a> &nbsp;
+    		<span className="footer__copyright">&copy; 2017 - Sparta Plaesant</span>&nbsp;-&nbsp;
+    		<a className="footer__social-link -instagram" href="#">Instagram</a>&nbsp;-&nbsp;
+    		<a className="footer__social-link -facebook" href="#">Facebook</a>&nbsp;-&nbsp;
     		<a className="footer__social-link -twitter" href="#">Twitter</a>
       </div>
     );
--- src/common/navigation/navigation.js
+++ src/common/navigation/navigation.js
@@ -1,17 +1,32 @@
 import React, { Component, Fragment } from 'react';
 
 class Navigation extends Component {
+
+  onNavClick = () => {
+    const element = document.querySelector(".navigation__button")
+    element.classList.toggle('active');
+
+    const overlay = document.querySelector(".navigation__overlay")
+    overlay.classList.toggle('active');
+  }
+
   render() {
     return (
       <div className="navigation">
       	<span className="navigation__phone">212.555.5555</span>
       	<a href="#" className="navigation__login">LOGIN</a>
 
-      	<a href="#" className="navigation__button">
+      	<div className="navigation__button" onClick={this.onNavClick}>
           <span className="navigation__button -top"></span>
           <span className="navigation__button -middle"></span>
           <span className="navigation__button -bottom"></span>
-        </a>
+        </div>
+
+        <div className="navigation__overlay">
+          <ul>
+            <li>Coming soon...</li>
+          </ul>
+        </div>
 
       </div>
     );
