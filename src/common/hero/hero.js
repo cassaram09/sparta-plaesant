fcbcfa6c0754d1363fda41095bd151e7c170264f
cassaram09 <cassaram09@gmail.com>
2019-03-13 08:49:46 -0400

    add text to all slides on hero carousel

--- src/common/hero/hero.js
+++ src/common/hero/hero.js
@@ -6,17 +6,55 @@ import Hero1 from 'assets/images/hero_1.png'
 import Hero2 from 'assets/images/hero_2.png'
 import Hero3 from 'assets/images/hero_3.png'
 
+const SLIDE_INFO = [
+  {
+    type: "Screening:",
+    title: "DVF Secret Agent Part 2",
+    date: "October 15, 2017",
+    subtitle: "Join us for a private screening of DVF Secret Agent Part 2 with our special guest, director Peter Lindberg.",
+    image: Hero1,
+    alt: "Runway"
+  },
+  {
+    type: "Screening:",
+    title: "DVF Secret Agent Part 3",
+    date: "October 16, 2017",
+    subtitle: "Join us for a private screening of DVF Secret Agent Part 3 with our special guest, director Peter Lindberg.",
+    image: Hero2,
+    alt: "Feet outside building"
+  },
+  {
+    type: "Screening:",
+    title: "DVF Secret Agent Part 4",
+    date: "October 17, 2017",
+    subtitle: "Join us for a private screening of DVF Secret Agent Part 4 with our special guest, director Peter Lindberg.",
+    image: Hero3,
+    alt: "Girls taking selfie"
+  }
+]
+
 class Hero extends Component {
+  renderSlides = () => {
+    return SLIDE_INFO.map(s => {
+      return (
+        <div className="hero__slide">
+          <img src={s.image} alt={s.alt} />
+          <div className="hero__text container">
+            <div className="hero__text__inner">
+              <h1>{s.type}</h1>
+              <h1>{s.title}</h1>
+              <h3>{s.date}</h3>
+              <p>{s.subtitle}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="hero">
-      	<div className="hero__text container">
-          <div className="hero__text__inner">
-        		<h1>Screening: <br/>DVF Secret Agent Part 2</h1>
-        		<h3>October 15, 2017</h3>
-        		<p>Join us for a private screening of DVF Secret Agent Part 2<br/>with our special guest, director Peter Lindberg.</p>
-          </div>
-      	</div>
        	<Carousel
        		showArrows={false}
        		showThumbs={false}
@@ -27,9 +65,7 @@ class Hero extends Component {
           transitionTime={500}
           stopOnHover={false}
        	>
-          <img src={Hero1} alt={"Runway photo"} />
-          <img src={Hero2} alt={"Feet outside building"} />
-          <img src={Hero3} alt={"Girls taking selfie"} />
+         {this.renderSlides()}
 	    	</Carousel>
       </div>
     );
